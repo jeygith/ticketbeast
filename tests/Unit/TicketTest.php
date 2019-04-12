68ec079356ed0c23b0dc246116b4194d021233c9
githire <geoffrey.mwaura@openworld.co.ke>
2019-04-12 11:07:43 +0300

    redundant and refactoring test case for order tickets release 3.6

---
+++ tests/Unit/TicketTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Concert;
+use App\Order;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TicketTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    /** @test */
+    function tickets_are_released_when_order_is_cancelled()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(10);
+
+        $order = $concert->orderTickets('jane@example.com', 5);
+
+
+        $this->assertEquals(5, $concert->ticketsRemaining());
+
+
+        $order->cancel();
+
+
+        $this->assertEquals(10, $concert->ticketsRemaining());
+
+        $this->assertNull(Order::find($order->id));
+
+
+    }
+
+
+    /** @test */
+    function a_ticket_can_be_released()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(1);
+        $order = $concert->orderTickets('jane@example.com', 1);
+
+        $ticket = $order->tickets()->first();
+
+        $this->assertEquals($order->id, $ticket->order_id);
+
+
+        $ticket->release();
+
+
+        $this->assertNull($ticket->fresh()->order_id);
+
+    }
+}

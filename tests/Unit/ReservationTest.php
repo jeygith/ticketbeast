9574cbe6e3f92eccc443c9f0a8a3c15d3abfef57
githire <geoffrey.mwaura@openworld.co.ke>
2019-04-16 13:23:50 +0300

    removing db dependencies on the reservation test class

--- tests/Unit/ReservationTest.php
+++ tests/Unit/ReservationTest.php
@@ -5,7 +5,9 @@ namespace Tests\Unit;
 use App\Concert;
 use App\Order;
 use App\Reservation;
+use App\Ticket;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Mockery;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,17 +15,18 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ReservationTest extends TestCase
 {
 
-    use DatabaseMigrations;
-
-
     /** @test */
     function calculating_the_total_cost()
     {
 
-        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(3);
-
-        $tickets = $concert->findTickets(3);
-
+        $this->disableExceptionHandling();
+        $tickets = collect(
+            [
+                (object)['price' => 1200],
+                (object)['price' => 1200],
+                (object)['price' => 1200]
+            ]
+        );
 
         $reservation = new Reservation($tickets);
 
@@ -32,65 +35,4 @@ class ReservationTest extends TestCase
     }
 
 
-    /** @test */
-    function creating_an_order_from_tickets_email_and_amount()
-    {
-
-        $concert = factory(Concert::class)->create()->addTickets(5);
-        $this->assertEquals(5, $concert->ticketsRemaining());
-
-
-        $order = Order::forTickets($concert->findTickets(3), 'john@example.com', 3600);
-
-
-        $this->assertEquals('john@example.com', $order->email);
-
-        $this->assertEquals(3, $order->ticketQuantity());
-        $this->assertEquals(3600, $order->amount);
-
-
-        $this->assertEquals(2, $concert->ticketsRemaining());
-
-
-    }
-
-    /** @test */
-    function converting_to_an_array()
-    {
-
-        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(10);
-
-        $order = $concert->orderTickets('jane@example.com', 5);
-
-
-        $result = $order->toArray();
-
-        $this->assertEquals([
-            'email' => 'jane@example.com',
-            'ticket_quantity' => 5,
-            'amount' => 6000
-        ], $result);
-    }
-
-
-    /** @test */
-    function tickets_are_released_when_order_is_cancelled()
-    {
-        $concert = factory(Concert::class)->create()->addTickets(10);
-
-        $order = $concert->orderTickets('jane@example.com', 5);
-
-
-        $this->assertEquals(5, $concert->ticketsRemaining());
-
-
-        $order->cancel();
-
-
-        $this->assertEquals(10, $concert->ticketsRemaining());
-
-        $this->assertNull(Order::find($order->id));
-
-
-    }
 }
